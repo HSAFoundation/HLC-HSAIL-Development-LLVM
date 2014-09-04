30864bf67db758083c9834be0627633c9c4faddb
Tom Stellard <thomas.stellard@amd.com>
2014-09-04 18:26:07 -0400

    lib/Support/ELF.h: Add EM_HSAIL and EM_HSAIL_64

--- include/llvm/Support/ELF.h
+++ include/llvm/Support/ELF.h
@@ -234,6 +234,8 @@ enum {
                           // Controller
   EM_CE            = 119, // Freescale Communication Engine RISC core
   EM_M32C          = 120, // Renesas M32C series microprocessors
+  EM_HSAIL         = 128, // HSAIL 32bit
+  EM_HSAIL_64      = 129, // HSAIL 64bit
   EM_TSK3000       = 131, // Altium TSK3000 core
   EM_RS08          = 132, // Freescale RS08 embedded processor
   EM_SHARC         = 133, // Analog Devices SHARC family of 32-bit DSP
