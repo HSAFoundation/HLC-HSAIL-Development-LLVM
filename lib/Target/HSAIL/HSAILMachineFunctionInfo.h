d8b9f8ff0931b1a5cb55beb0b356fcc60a3be410
foreman <dl.constructicon@amd.com>
2014-10-09 19:16:53 -0400

    P4 to Git Change 1086148 by marsenau@marsenau_home on 2014/10/09 18:41:17
    	ECR #304775 - Remove dead code copied from AMDIL. Most of the remaining HSAILMachineFunctionInfo is not dead, but should be since HSAIL doesn't have any of these things.
    
    Affected files ...
    
    ... //depot/stg/opencl/drivers/opencl/compiler/llvm/lib/Target/HSAIL/BRIGAsmPrinter.cpp#93 edit
    ... //depot/stg/opencl/drivers/opencl/compiler/llvm/lib/Target/HSAIL/BRIGAsmPrinter.h#38 edit
    ... //depot/stg/opencl/drivers/opencl/compiler/llvm/lib/Target/HSAIL/HSAILMachineFunctionInfo.cpp#8 edit
    ... //depot/stg/opencl/drivers/opencl/compiler/llvm/lib/Target/HSAIL/HSAILMachineFunctionInfo.h#7 edit

--- lib/Target/HSAIL/HSAILMachineFunctionInfo.h
+++ lib/Target/HSAIL/HSAILMachineFunctionInfo.h
@@ -94,14 +94,9 @@ namespace llvm
   } SamplerInfo;
   // Some typedefs that will help with using the various iterators
   // of the machine function info class.
-  typedef std::map<uint32_t, uint32_t>::iterator lit32_iterator;
-  typedef std::map<uint64_t, uint32_t>::iterator lit64_iterator;
-  typedef std::map<std::pair<uint64_t, uint64_t>, uint32_t>::iterator
-    lit128_iterator;
   typedef StringMap<SamplerInfo>::iterator sampler_iterator;
   typedef DenseSet<uint32_t>::iterator func_iterator;
   typedef DenseSet<uint32_t>::iterator intr_iterator;
-  typedef DenseSet<uint32_t>::iterator uav_iterator;
   typedef DenseSet<uint32_t>::iterator sema_iterator;
   typedef DenseSet<uint32_t>::iterator read_image1d_iterator;
   typedef DenseSet<uint32_t>::iterator write_image1d_iterator;
@@ -125,48 +120,6 @@ namespace llvm
   // hsail target-specific information for each MachineFunction
   class HSAILMachineFunctionInfo : public MachineFunctionInfo
   {
-    // CalleeSavedFrameSize - Size of the callee-saved
-    // register portion of the
-    // stack frame in bytes.
-    unsigned int CalleeSavedFrameSize;
-    // BytesToPopOnReturn - Number of bytes function pops on return.
-    // Used on windows platform for stdcall & fastcall name decoration
-    unsigned int BytesToPopOnReturn;
-    // DecorationStyle - If the function requires additional
-    // name decoration,
-    // DecorationStyle holds the right way to do so.
-    NameDecorationStyle DecorationStyle;
-    // ReturnAddrIndex - FrameIndex for return slot.
-    int ReturnAddrIndex;
-
-    // TailCallReturnAddrDelta - Delta the ReturnAddr stack slot is moved
-    // Used for creating an area before the register spill area
-    // on the stack
-    // the returnaddr can be savely move to this area
-    int TailCallReturnAddrDelta;
-
-    // SRetReturnReg - Some subtargets require that sret lowering includes
-    // returning the value of the returned struct in a register.
-    // This field holds the virtual register into which the sret
-    // argument is passed.
-    unsigned int SRetReturnReg;
-
-    // UsesLocal - Specifies that this function uses LDS memory and
-    // that it needs to be allocated.
-    bool UsesLDS;
-
-    // LDSArg - Flag that specifies if this function has an Local
-    // argument or not
-    bool LDSArg;
-
-    // UsesGDS - Specifies that this function uses GDS memory and
-    // that it needs to be allocated.
-    bool UsesGDS;
-
-    // GDSArg - Flag that specifies if this function has an Region
-    // argument or not
-    bool GDSArg;
-
     // The size in bytes required to host all of the kernel arguments.
     // -1 means this value has not been determined yet.
     int32_t mArgSize;
@@ -193,25 +146,10 @@ namespace llvm
     /// smaller literals in the current function.
     std::map<uint32_t, uint32_t> mIntLits;
 
-    /// A map of constant to literal mapping for all of the 64bit
-    /// literals in the current function.
-    std::map<uint64_t, uint32_t> mLongLits;
-
-    /// A map of constant to literal mapping for all of the 128bit
-    /// literals in the current function.
-    std::map<std::pair<uint64_t, uint64_t>, uint32_t> mVecLits;
-
-    /// The number of literals that should be reserved.
-    /// TODO: Remove this when the wrapper emitter is added.
-    uint32_t mReservedLits;
-
     /// A map of name to sampler information that is used to emit
     /// metadata to the IL stream that the runtimes can use for
     /// hardware setup.
     StringMap<SamplerInfo> mSamplerMap;
-
-    /// Array of flags to specify if a specific memory type is used or not.
-    bool mUsedMem[HSAILDevice::MAX_IDS];
 
     /// Set of all functions that this function calls.
     DenseSet<uint32_t> mFuncs;
@@ -221,8 +159,6 @@ namespace llvm
 
     /// Set of all the raw uavs.
     DenseSet<uint32_t> mRawUAV;
-    /// Set of all the arena uavs.
-    DenseSet<uint32_t> mArenaUAV;
 
     /// Set of all semaphores
     DenseSet<uint32_t> mSemaphore;
@@ -256,29 +192,13 @@ namespace llvm
 
     explicit HSAILMachineFunctionInfo(MachineFunction &MF);
 
-    unsigned int getCalleeSavedFrameSize() const;
-    void setCalleeSavedFrameSize(unsigned int bytes);
+    // FIXME: Remove these
+    void setUsesLocal() { }
+    void setUsesRegion() { }
 
-    unsigned int getBytesToPopOnReturn() const;
-    void setBytesToPopOnReturn (unsigned int bytes);
-
-    NameDecorationStyle getDecorationStyle() const;
-    void setDecorationStyle(NameDecorationStyle style);
-
-    int getRAIndex() const;
-    void setRAIndex(int Index);
-
-    int getTCReturnAddrDelta() const;
-    void setTCReturnAddrDelta(int delta);
-
-    unsigned int getSRetReturnReg() const;
-    void setSRetReturnReg(unsigned int Reg);
-
-    void setUsesLocal();
-    void setHasLocalArg();
-
-    void setUsesRegion();
-    void setHasRegionArg();
+    bool usesHWConstant(std::string name) const;
+    bool isKernel() const;
+    HSAILKernel* getKernel();
 
     /// Get the size in bytes that are required to host all of
     /// arguments and stack memory in scratch.
@@ -307,50 +227,9 @@ namespace llvm
     // uint32_t addi32Literal(uint32_t val, int Opcode = HSAIL::LOADCONST_i32);
     uint32_t addi32Literal(uint32_t val, int Opcode = 0);
 
-    /// Add a 32bit floating point value to the literal table.
-    uint32_t addf32Literal(const ConstantFP *CFP);
-
-    /// Add a 32bit floating point value to the literal table.
-    uint32_t addf32Literal(uint32_t val);
-
-    /// Add a 64bit integer value to the literal table.
-    uint32_t addi64Literal(uint64_t val);
-
-    /// Add a 128 bit integer value to the literal table.
-    uint32_t addi128Literal(uint64_t val_lo, uint64_t val_hi);
-
-    /// Add a 64bit floating point literal as a 64bit integer value.
-    uint32_t addf64Literal(const ConstantFP *CFP);
-
-    /// Add a 64bit floating point literal as a 64bit integer value.
-    uint32_t addf64Literal(uint64_t val);
-
-    /// Get the number of literals that have currently been allocated.
-    size_t getNumLiterals() const;
-
-    /// Get the literal ID of an Integer literal of the given offset.
-    uint32_t getIntLits(uint32_t lit);
-
-    /// Get the literal ID of a Long literal of the given offset.
-    uint32_t getLongLits(uint64_t lit);
-
-    /// Get the literal ID of a Long literal of the given offset.
-    uint32_t getVecLits(uint64_t low64, uint64_t high64);
-
-    /// Add some literals to the number of reserved literals.
-    void addReservedLiterals(uint32_t);
-
-    // Add a sampler to the set of known samplers for the current kernel.
-    uint32_t addSampler(std::string name, uint32_t value);
-
     // Iterators that point to the beginning and end of the sampler map.
     sampler_iterator sampler_begin() { return mSamplerMap.begin(); }
     sampler_iterator sampler_end() { return mSamplerMap.end(); }
-
-    /// Set the flag for the memory ID to true for the current function.
-    void setUsesMem(unsigned);
-    /// Retrieve the flag for the memory ID.
-    bool usesMem(unsigned);
 
     /// Add called functions to the set of all functions this function calls.
     void addCalledFunc(uint32_t id) { mFuncs.insert(id); }
@@ -361,7 +240,6 @@ namespace llvm
     func_iterator func_end() { return mFuncs.end(); }
 
     /// Add called intrinsics to the set of all intrinscis this function calls.
-    void addCalledIntr(uint32_t id) { mIntrs.insert(id); }
     size_t intr_size() { return mIntrs.size(); }
     bool intr_empty() { return mIntrs.empty(); }
     intr_iterator intr_begin() { return mIntrs.begin(); }
@@ -376,24 +254,11 @@ namespace llvm
 
     /// Add a raw uav id.
     void uav_insert(uint32_t id) { mRawUAV.insert(id); }
-    bool uav_count(uint32_t id) { return mRawUAV.count(id); }
-    size_t uav_size() { return mRawUAV.size(); }
-    uav_iterator uav_begin() { return mRawUAV.begin(); }
-    uav_iterator uav_end() { return mRawUAV.end(); }
-
-    /// Add an arena uav id.
-    void arena_insert(uint32_t id) { mArenaUAV.insert(id); }
-    bool arena_count(uint32_t id) { return mArenaUAV.count(id); }
-    size_t arena_size() { return mArenaUAV.size(); }
-    uav_iterator arena_begin() { return mArenaUAV.begin(); }
-    uav_iterator arena_end() { return mArenaUAV.end(); }
 
     /// Add a pointer to the known set of read-only pointers
     void add_read_ptr(const Value* ptr) { mReadPtr.insert(ptr); }
     bool read_ptr_count(const Value* ptr) { return mReadPtr.count(ptr); }
     bool read_size() { return mReadPtr.size(); }
-    read_ptr_iterator read_ptr_begin() { return mReadPtr.begin(); } 
-    read_ptr_iterator read_ptr_end() { return mReadPtr.end(); }
 
     // Add an error to the output for the current function.
     typedef enum {
