241e47c56ef1d1dd2092cd45d8fb5b5b721d5a61
Matt Arsenault <Matthew.Arsenault@amd.com>
2014-10-27 16:22:08 -0700

    Add stub HSAIL target streamer

---
+++ lib/Target/HSAIL/MCTargetDesc/HSAILTargetStreamer.cpp
@@ -0,0 +1,20 @@
+//===- HSAILTargetStreamer.cpp ---------------------------*- C++ -*---------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the HSAILTargetStreamer class.
+//
+//===----------------------------------------------------------------------===//
+#include "HSAILTargetStreamer.h"
+
+using namespace llvm;
+
+HSAILTargetStreamer::HSAILTargetStreamer(MCStreamer &S)
+  : MCTargetStreamer(S) {}
+
+HSAILTargetStreamer::~HSAILTargetStreamer() {}
