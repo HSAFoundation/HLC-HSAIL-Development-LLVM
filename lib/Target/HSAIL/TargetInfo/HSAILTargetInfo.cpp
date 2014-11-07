6f39ddd7aad6726a3f5f868a54a647b444210918
Matt Arsenault <Matthew.Arsenault@amd.com>
2014-11-06 18:14:25 -0800

    Change arch name to be hsail64 instead of hsail-64
    This needs to match the triple arch name. This was failing to match,
    so this would default to the host architecture and triple. This just
    happens to work correctly on Linux. For some reason, on OS X this would
    result in some kind of inconsistency trying to use ELF sometimes while
    set up for MachO, so hsail64 would always crash.

--- lib/Target/HSAIL/TargetInfo/HSAILTargetInfo.cpp
+++ lib/Target/HSAIL/TargetInfo/HSAILTargetInfo.cpp
@@ -19,6 +19,6 @@ extern "C" void LLVMInitializeHSAILTargetInfo()
   RegisterTarget<Triple::hsail, /*HasJIT=*/false>
     X(TheHSAIL_32Target, "hsail", "32-bit HSAIL: small machine model, addresses are 32 bit");
 
-  RegisterTarget<Triple::hsail_64, /*HasJIT=*/false>
-    Y(TheHSAIL_64Target, "hsail-64", "64-bit HSAIL: large machine model, addresses are 64 bit");
+  RegisterTarget<Triple::hsail64, /*HasJIT=*/false>
+    Y(TheHSAIL_64Target, "hsail64", "64-bit HSAIL: large machine model, addresses are 64 bit");
 }
